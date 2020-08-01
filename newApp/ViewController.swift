8bf0b87ba032416a815945b1feaa85205fc1edd7
Ahmed kamal <Ahmedkamal121995@gmail.com>
2020-08-02 01:33:50 +0200

    Initial Commit

---
+++ newApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newApp
+//
+//  Created by Ahmed kamal on 8/2/20.
+//  Copyright © 2020 Ahmed kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
